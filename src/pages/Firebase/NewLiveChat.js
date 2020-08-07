553dcbfc8241ad8e9555c92bdf5a7dd86255e66f
Nidhin BM <nidhinbm.bm@gmail.com>
2020-08-07 14:26:51 +0530

    iivechat

--- src/pages/Firebase/NewLiveChat.js
+++ src/pages/Firebase/NewLiveChat.js
@@ -36,70 +36,69 @@ class NewLiveChat extends Component {
     backgroundColor: "white",
     height: "70px",
     width: "100%",
-    fontSize: "20px",
-    boxShadow: "#0a0a0a 0px -1px -11px 0px",
+    fontSize:"20px",
+    boxShadow: "#0a0a0a 0px -1px -11px 0px"
   };
   render() {
     return (
       <div>
-        <div className="d-none d-lg-block">
-          <form onSubmit={this.handleSubmit}>
-            <div class="input-group mb-3">
-              <input
-                type="text"
-                placeholder="Type and Press Enter to Chat"
-                className="form-control"
-                ref="comment"
-                // id="exampleFormControlTextarea1"
-                id="validationDefault01"
-                rows="1"
-                onKeyDown={this.handleEnter}
-                required
-                maxLength="500"
-                aria-describedby="button-addon2"
-                autoFocus={true}
-              />
-              <div class="input-group-append">
-                <button
-                  class="btn btn-outline-secondary"
-                  type="submit"
-                  id="button-addon2"
-                >
-                  Post
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
-        <div className="d-lg-none p-3" style={this.style}>
-          <form onSubmit={this.handleSubmit}>
-            <div class="input-group mb-3">
-              <input
-                type="text"
-                placeholder={"Chat as " + this.props.user.displayName}
-                className="form-control"
-                ref="comment"
-                // id="exampleFormControlTextarea1"
-                id="validationDefault01"
-                rows="1"
-                onKeyDown={this.handleEnter}
-                required
-                maxLength="500"
-                aria-describedby="button-addon2"
-                autoFocus={true}
-              />
-              <div class="input-group-append">
-                <button
-                  class="btn btn-outline-dark"
-                  type="submit"
-                  id="button-addon2"
-                >
-                  Post
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
+       
+                     <div className="d-none d-lg-blockz">
+                     <form onSubmit={this.handleSubmit}>
+        <div class="input-group mb-3">
+        <input
+        type="text"
+          placeholder="Type and Press Enter to Chat"
+          className="form-control"
+          ref="comment"
+          // id="exampleFormControlTextarea1"
+          id="validationDefault01"
+          rows="1"
+          onKeyDown={this.handleEnter}
+          required
+          maxLength="500"
+          aria-describedby="button-addon2"
+          autoFocus={true}
+         
+        />
+  <div class="input-group-append">
+    <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Post</button>
+  </div>
+</div>
+        
+      
+      </form>
+                     </div>
+                     <div className="d-lg-nonez p-3" style={this.style}>
+                      
+                     <form onSubmit={this.handleSubmit}>
+        <div class="input-group mb-3">
+        <input
+        type="text"
+          placeholder={'Chat as ' + this.props.user.displayName}
+          className="form-control"
+          ref="comment"
+          // id="exampleFormControlTextarea1"
+          id="validationDefault01"
+          rows="1"
+          onKeyDown={this.handleEnter}
+          required
+          maxLength="500"
+          aria-describedby="button-addon2"
+          autoFocus={true}
+          style={{ backgroundColor:"#ffffff"}}
+         
+        />
+  <div class="input-group-append">
+    <button class="btn btn-outline-dark" type="submit" id="button-addon2">Post</button>
+  </div>
+</div>
+        
+      
+      </form>
+
+                     </div>
+    
       </div>
     );
   }
