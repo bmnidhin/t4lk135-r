2deee03c0ca92af7ca9d4bfb05e408c28fc25e77
Nidhin BM <nidhinbm.bm@gmail.com>
2021-04-22 23:20:57 +0530

    way

--- src/pages/homePageComponents/HeroText.js
+++ src/pages/homePageComponents/HeroText.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import * as SETTINGS from '../constants/Settings';
+// import randomQuotes from 'random-quotes';
 
 export default class HeroText extends Component {
   constructor(props) {
     super(props);
-
+    this.state ={
+      // quote:randomQuotes()
+    }
     this.textStyle = {  
       color: "white",
       paddingLeft: "40px",
@@ -16,8 +19,10 @@ export default class HeroText extends Component {
     return (
       <div className="pt-5 pb-4">
         <h1 style={this.textStyle} className="Herotext">
+          {/* {this.state.quote.body} */}
           {SETTINGS.heroText}
         </h1>
+        {/* <span className="text-muted">{this.state.quote.author}</span> */}
       </div>
     );
   }
