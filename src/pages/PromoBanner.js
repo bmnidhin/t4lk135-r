06e1c339f011ccc1600be7af65299512bf487c02
Nidhin BM <nidhinbm.bm@gmail.com>
2021-05-21 23:14:41 +0530

    Update PromoBanner.js

--- src/pages/PromoBanner.js
+++ src/pages/PromoBanner.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 export default class PromoBanner extends Component {
     render() {
-        return (
-            <div className="pt-3 pb-3">
+        return (""
+            {/* <div className="pt-3 pb-3">
                 <Link to={'/athena21'} target="_blank">
                 <img src="https://thetkmshow.github.io/static/poster/athena.png" className="border border-warning roundedImage" width="100%"/>
                 </Link>
-            </div>
+            </div>*/}
         )
     }
 }
